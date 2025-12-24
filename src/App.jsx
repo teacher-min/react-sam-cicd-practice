4c08b98ea3d03ccc6ecf3fa030ad8999214714c3
teacher-min <minkt78@gmail.com>
2025-12-24 02:06:48 +0000

    [Fix] something

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 function App() {
-  const [serverTime, setServerTime] = useState('시간을 불러와주세요')
+  const [serverTime, setServerTime] = useState('현재 시간을 확인해 보세요')
 
   // [중요] 여기에 Backend 배포 후 얻은 URL을 넣습니다.
   const API_URL = "https://y8uwis0swh.execute-api.ap-northeast-2.amazonaws.com/Prod/time";
@@ -20,7 +20,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Serverless Time Check</h1>
-      <button onClick={fetchTime}>지금 몇 시?</button>
+      <button onClick={fetchTime}>시간 확인하기</button>
       <h2 style={{ color: 'blue' }}>{serverTime}</h2>
     </div>
   )
