539cdc1d4127f1bd9bc389d702cd1dec0ba3e365
teacher-min <minkt78@gmail.com>
2025-12-24 01:43:30 +0000

    First React commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+function App() {
+  const [serverTime, setServerTime] = useState('시간을 불러와주세요')
+
+  // [중요] 여기에 Backend 배포 후 얻은 URL을 넣습니다.
+  const API_URL = "hhttps://y8uwis0swh.execute-api.ap-northeast-2.amazonaws.com/Prod/time";
+
+  const fetchTime = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setServerTime(data.time);
+    } catch (error) {
+      console.error(error);
+      setServerTime("에러 발생!");
+    }
+  }
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Serverless Time Check</h1>
+      <button onClick={fetchTime}>지금 몇 시?</button>
+      <h2 style={{ color: 'blue' }}>{serverTime}</h2>
+    </div>
+  )
+}
+export default App
